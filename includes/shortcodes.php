f0bda6601e0cd7b2e71754ffeafb5d7ff4d40cbc
r0b1n1sl4m <r0b1n1sl4m@gmail.com>
2018-01-29 13:59:40 +0600

    Dropped uneccessery shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -7,21 +7,16 @@ class Shortcodes {
 	public function __construct() {
 
 		// Register shortcode
-		add_shortcode( 'hello_world', array( $this, 'hello_world_shortcode' ) );
 		add_shortcode( 'current_datetime', array ( $this, 'current_datetime_shortcode' ) );
 
 	}
 
 
 	/**
-	* Hello World! shortcode handler.
+	* Current date time shortcode output handler.
+	*
+	* @return string Current date time.
 	*/
-	public function hello_world_shortcode() {
-
-		return '<span style="color:red;">Hello World!</span><br/>';
-
-	}
-
 	public function current_datetime_shortcode() {
 
 		// Return current datetime
